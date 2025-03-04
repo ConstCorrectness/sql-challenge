414089ce10d8e3a4dbf9810ac06c964eaa075ee6
horriblecpp <horribleccprogram@proton.me>
2025-03-03 22:37:10 -0500

    if not exists

--- schemas/all_schemas.sql
+++ schemas/all_schemas.sql
@@ -1,14 +1,14 @@
-CREATE TABLE departments (
+CREATE TABLE IF NOT EXISTS departments (
     dept_no       char(4) PRIMARY KEY,
     dept_name     varchar(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE titles (
+CREATE TABLE IF NOT EXISTS titles (
     title_id    char(5) PRIMARY KEY,
     title       varchar(50) NOT NULL
 );
 
-CREATE TABLE employees (
+CREATE TABLE IF NOT EXISTS employees (
     emp_no        int PRIMARY KEY,
     emp_title_id  char(5) NOT NULL,
     birth_date    date NOT NULL,
@@ -19,14 +19,14 @@ CREATE TABLE employees (
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
-CREATE TABLE salaries (
+CREATE TABLE IF NOT EXISTS salaries (
     emp_no        int,
     salary        int NOT NULL,
     PRIMARY KEY   (emp_no, salary),                         -- composite key, salary updates
     FOREIGN KEY   (emp_no) REFERENCES employees(emp_no)
 );
 
-CREATE TABLE dept_emp (
+CREATE TABLE IF NOT EXISTS dept_emp (
     emp_no      int,
     dept_no     char(4),
     PRIMARY KEY (emp_no, dept_no),                          -- composite primary key
@@ -34,7 +34,7 @@ CREATE TABLE dept_emp (
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-CREATE TABLE dept_manager (
+CREATE TABLE IF NOT EXISTS dept_manager (
     emp_no      int,
     dept_no     char(4),
     PRIMARY KEY (emp_no, dept_no),                              -- composite primary key
